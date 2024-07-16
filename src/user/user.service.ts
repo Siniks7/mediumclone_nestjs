e61314b563787ae970f17c9d6c66f83024d8d9df
Nikolay <siniks7@yandex.ru>
2024-07-16 23:35:34 +0700

    fix jwt

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,7 @@
+import { JWT_SECRET } from '@app/config';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Buffer } from 'buffer';
-import { generateKeyPairSync, sign } from 'crypto';
+import { sign } from 'jsonwebtoken';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/createUser.dto';
 import { UserResponseInterface } from './types/userResponse.interface';
@@ -20,18 +20,18 @@ export class UserService {
   }
 
   generateJwt(user: UserEntity): string {
-    const { privateKey } = generateKeyPairSync('rsa', {
-      modulusLength: 2048
-    });
-    const thisUser = {
-      id: user.id,
-      username: user.username,
-      email: user.email
+    const options = {
+      expiresIn: '10m'
     };
-    const data = Buffer.from(JSON.stringify(thisUser));
-    const sign1 = sign('SHA256', data, privateKey);
-    const signature = sign1.toString('base64');
-    return signature;
+    return sign(
+      {
+        id: user.id,
+        username: user.username,
+        email: user.email
+      },
+      JWT_SECRET,
+      options
+    );
   }
 
   buildUserResponse(user: UserEntity): UserResponseInterface {
