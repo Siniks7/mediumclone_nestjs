04fa704df7755164641fbd93e4041a907d44b37a
Nikolay <siniks7@yandex.ru>
2024-07-22 22:45:34 +0700

    Создание метода статьи

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,13 +1,22 @@
+import { User } from '@app/user/decorators/user.decorator';
 import { AuthGuard } from '@app/user/guards/auth.guard';
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import { UserEntity } from '@app/user/user.entity';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ArticleService } from './article.service';
+import { CreateArticleDto } from './dto/createArticle.dto';
 
 @Controller('articles')
 export class ArticleController {
   constructor(private readonly articleService: ArticleService) {}
   @Post()
   @UseGuards(AuthGuard)
-  async create() {
-    return this.articleService.createArticle();
+  async create(
+    @User() currentUser: UserEntity,
+    @Body('article') createArticleDto: CreateArticleDto
+  ): Promise<any> {
+    return await this.articleService.createArticle(
+      currentUser,
+      createArticleDto
+    );
   }
 }
--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -2,6 +2,7 @@ import { ConnectionOptions } from 'typeorm';
 import { CreateTags1720936120572 } from './migrations/1720936120572-CreateTags';
 import { CreateUser1721053657921 } from './migrations/1721053657921-CreateUser';
 import { CreateArticles1721660789183 } from './migrations/1721660789183-CreateArticles';
+import { AddRelationsBetweenArticleAndUser1721661461034 } from './migrations/1721661461034-AddRelationsBetweenArticleAndUser';
 
 // __dirname + '/migrations/**/*{.ts,.js}
 const config: ConnectionOptions = {
@@ -16,7 +17,8 @@ const config: ConnectionOptions = {
   migrations: [
     CreateTags1720936120572,
     CreateUser1721053657921,
-    CreateArticles1721660789183
+    CreateArticles1721660789183,
+    AddRelationsBetweenArticleAndUser1721661461034
   ],
   cli: {
     migrationsDir: 'src/migrations'
